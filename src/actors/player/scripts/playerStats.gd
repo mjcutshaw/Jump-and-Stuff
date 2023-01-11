c3c5cb5551f859f3e943c06076a88afeadd4184f
Matt <mjcutshaw@gmail.com>
2023-01-11 02:15:57 -0500

    faster

--- src/actors/player/scripts/playerStats.gd
+++ src/actors/player/scripts/playerStats.gd
@@ -4,7 +4,7 @@ class_name PlayerStats
 var healthMax: int = 4
 var health: int = 3
 
-var baseSpeed: int = 20
+var baseSpeed: int = 25
 var turboSpeed #min speed for wall run  #TODO: make based off base speed
 
 var baseJumpHeight: float = 4.5
